28a9f32dad98e9c21e67d7236c71250c06f04b06
cvoscode <82336622+cvoscode@users.noreply.github.com>
2023-03-05 23:26:01 +0100

    Update styling.py

--- utils/styling.py
+++ utils/styling.py
@@ -1,13 +1,32 @@
 import dash_bootstrap_components as dbc 
-from dash_bootstrap_templates import load_figure_template
-
+import plotly.io as pio
+import plotly.express as px
+import plotly.graph_objects as go
+pio.templates["draft"] = go.layout.Template(
+    layout_annotations=[
+        dict(
+            name="draft watermark",
+            #Here you can input a text for a watermark
+            text="Leck Mich es is spät",
+            textangle=-30,
+            opacity=0.1,
+            font=dict(color="black", size=100),
+            xref="paper",
+            yref="paper",
+            x=0.5,
+            y=0.5,
+            showarrow=False,
+        )
+    ],
+)
 
 
 def style_app():
     """styling of the app
     """
     external_style=dbc.themes.SKETCHY
-    figure_template=load_figure_template("sketchy")
     colors={'Sucess':'Green','Error':'Red'}
+    color_scale=['rgb(0, 0, 0)','rgb(0, 255, 255)']
     min_style={'margin':'2px'}
-    return external_style, figure_template,colors,min_style
+    return external_style,colors,min_style,color_scale
+
