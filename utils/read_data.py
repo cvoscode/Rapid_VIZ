4fd169fd52f859743d5a768567c017b1c4751ae6
cvoscode <82336622+cvoscode@users.noreply.github.com>
2023-03-05 11:18:05 +0100

    Update read_data.py

--- utils/read_data.py
+++ utils/read_data.py
@@ -1,10 +1,14 @@
 import pandas as pd 
-
+import os
 def read_data(PATH):
-    try:
+    filename,ext=os.path.splitext(PATH)
+    if ext=='.csv':
         df=pd.read_csv(PATH)
         return df
-    except:
-        return Exception
+    if ext=='.parquet':
+        df=pd.read_parquet(PATH)
+        return df
+    if ext=='.xlsx':
+        df=pd.read_excel(PATH)
 
    
