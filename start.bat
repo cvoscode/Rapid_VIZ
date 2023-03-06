abce4aba815e6ac723dbe7e3025f3a5b6ceeed4d
cvoscode <82336622+cvoscode@users.noreply.github.com>
2023-03-06 23:31:30 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -7,7 +7,7 @@ call rvizenv\Scripts\activate
 
 start http://127.0.0.1:8080/
 
-echo If the app loads automatically please refresh
+echo If the app does not load automatically please refresh
 
 waitress-serve app:app.server
 
